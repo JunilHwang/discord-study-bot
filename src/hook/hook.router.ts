3b1c0582740ea75e642772d7d5033c92ff0655a6
junilhwang <tjsdlf4261@naver.com>
2020-07-02 01:14:17 +0900

    fix: request.header => headers 변경

--- src/hook/hook.router.ts
+++ src/hook/hook.router.ts
@@ -1,13 +1,14 @@
 import express, {Request, Response} from 'express';
 import { GithubHook } from "../github/github.domain";
+import {IncomingHttpHeaders} from "http";
 
 const router = express.Router();
 
 router.post('/api/github/hook', (request: Request, response: Response) => {
-  const header = request.header;
+  const headers: IncomingHttpHeaders = request.headers;
   const payload: GithubHook = request.body;
   console.log(payload);
-  console.log(header);
+  console.log(headers);
   response.send('success');
 })
 
