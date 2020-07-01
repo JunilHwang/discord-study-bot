dc7c03429e5810c0a21b5bfa59a96ae892cf4195
junilhwang <tjsdlf4261@naver.com>
2020-07-02 01:12:31 +0900

    fix: export 개선

--- src/hook/hook.router.ts
+++ src/hook/hook.router.ts
@@ -1,15 +1,14 @@
 import express, {Request, Response} from 'express';
 import { GithubHook } from "../github/github.domain";
 
-const hookRouter = express.Router();
+const router = express.Router();
 
-hookRouter.post('/api/github/hook', (request: Request, response: Response) => {
+router.post('/api/github/hook', (request: Request, response: Response) => {
   const header = request.header;
   const payload: GithubHook = request.body;
+  console.log(payload);
   console.log(header);
   response.send('success');
 })
 
-export {
-  hookRouter
-};
+export default router;
