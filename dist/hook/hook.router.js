897a96f3d4264bf78109c8a893dbcf1158869e68
junilhwang <tjsdlf4261@naver.com>
2020-07-03 23:32:10 +0900

    feat: gitignore 수정

--- dist/hook/hook.router.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const hookRouter = express.Router();
-hookRouter.post('/api/github/hook', (request, response) => {
-    const header = request.header;
-    const payload = request.body;
-    response.send('success');
-});
-export { hookRouter };
