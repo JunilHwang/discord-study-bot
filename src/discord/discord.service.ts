49f9cdff66c8b4e16cdec03e1c9e382746903ed9
junilhwang <tjsdlf4261@naver.com>
2020-07-03 17:46:42 +0900

    feat: discrod service 추가 완료

---
+++ src/discord/discord.service.ts
@@ -0,0 +1,21 @@
+import {token} from './discord.secret';
+import axios from 'axios';
+
+const DISCORD_API_URL = 'https://discord.com/api/v6';
+
+class DiscordService {
+  constructor() {
+    this.channelURL = DISCORD_API_URL + '/channels/728516777562079244';
+    this.headers = {
+      Authorization: `Bot ${token}`
+    };
+  }
+
+  sendMessage(content) {
+    const requestBody = { content, tts: false };
+    const url = `${this.channelURL}/messages`;
+    return axios.post(url, requestBody, { headers });
+  }
+}
+
+export default new DiscordService();
