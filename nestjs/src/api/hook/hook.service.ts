9e31a525bb8643b4f18358b72ea381f80b45c463
junilhwang <tjsdlf4261@naver.com>
2020-07-05 21:40:55 +0900

    feat: hook.service 리팩토링 중(1)

--- nestjs/src/api/hook/hook.service.ts
+++ nestjs/src/api/hook/hook.service.ts
@@ -28,15 +28,17 @@ export class HookService {
     const method: string|undefined = types[type];
     if (method === undefined) return;
     try {
-      return this[method](body).catch((e: any) => { throw e; })
+      return this.discordService
+                 .sendMessage(this[method](body))
+                 .catch((e: any) => { throw e; })
     } catch (e) {
       console.error('HookService.selectHookType()', e);
     }
   }
 
-  public createPushMessage (body: any): Promise<AxiosResponse> {
+  public createPushMessage (body: any): string {
     const data: GithubPushHook = body as GithubPushHook;
-    const message: string = [
+    return [
       `[저장소에 푸쉬 발생]`,
       `Repository: __**${data.repository.name}**__`,
       `url: __**${data.repository.html_url}**__`,
@@ -44,12 +46,11 @@ export class HookService {
       `\n[커밋정보]`,
       data.commits.map(commit => `${commit.message}        ${commit.timestamp}`).join('\n'),
     ].join('\n');
-    return this.discordService.sendMessage(message);
   }
 
-  public createPRMessage (body: any): Promise<AxiosResponse> {
+  public createPRMessage (body: any): string {
     const data: GithubPRHook = body as GithubPRHook;
-    const message: string = [
+    return [
       `[Pull Request]`,
       `action: __**${data.action}**__`,
       `url: __**${data.pull_request.html_url}**__`,
@@ -59,12 +60,11 @@ export class HookService {
       `--------------------------`,
       `created_at: ${data.pull_request.created_at}`,
     ].join('\n');
-    return this.discordService.sendMessage(message);
   }
 
-  public createPRReviewMessage (body: any): Promise<AxiosResponse> {
+  public createPRReviewMessage (body: any): string {
     const data: GithubPRReviewHook = body as GithubPRReviewHook;
-    const message: string = [
+    return [
       `[Pull Request Review]`,
       `action: __**${data.action}**__`,
       `url: __**${data.pull_request.html_url}**__`,
@@ -73,12 +73,11 @@ export class HookService {
       `reviewer: __**${data.review.user.login}**__`,
       data.review.body,
     ].join('\n');
-    return this.discordService.sendMessage(message);
   }
 
-  public createPRReviewCommentMessage (body: any): Promise<AxiosResponse> {
+  public createPRReviewCommentMessage (body: any): string {
     const data: GithubPRReviewCommentHook = body as GithubPRReviewCommentHook;
-    const message: string = [
+    return [
       `[Pull Request Review Comment]`,
       `action: __**${data.action}**__`,
       `url: __**${data.pull_request.html_url}**__`,
@@ -87,12 +86,11 @@ export class HookService {
       `writer: __**${data.comment.user.login}**__`,
       data.comment.body
     ].join('\n');
-    return this.discordService.sendMessage(message);
   }
 
-  public createIssueMessage (body: any): Promise<AxiosResponse> {
+  public createIssueMessage (body: any): string {
     const data: GithubIssueHook = body as GithubIssueHook;
-    const message: string = [
+    return [
       `[Issue]`,
       `action: __**${data.action}**__`,
       `writer: __**${data.issue.user.login}**__`,
@@ -100,18 +98,16 @@ export class HookService {
       `url: __**${data.issue.html_url}**__`,
       data.issue.body,
     ].join('\n');
-    return this.discordService.sendMessage(message);
   }
 
-  public createIssueCommentMessage (body: any): Promise<AxiosResponse> {
+  public createIssueCommentMessage (body: any): string {
     const data: GithubIssueCommentHook = body as GithubIssueCommentHook;
-    const message: string = [
+    return [
       `[Issue Comment]`,
       `writer: __**${data.comment.user.login}**__`,
       `created_at: __**${data.comment.created_at}**__`,
       `url: __**${data.comment.html_url}**__`,
       data.comment.body,
     ].join('\n');
-    return this.discordService.sendMessage(message);
   }
 }
