3c001304086a3816c6fe6973d378b425d00d5188
junilhwang <tjsdlf4261@naver.com>
2020-07-06 22:19:00 +0900

    feat: [cms] element-ui 추가

--- cms/src/main.ts
+++ cms/src/main.ts
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './styles/index.scss'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './styles/index.scss'
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
