54f53be18c0fc90def7ef943c19464c89d8524e6
junilhwang <tjsdlf4261@naver.com>
2020-07-06 22:19:22 +0900

    feat: [cms] Sidebar 추가

--- cms/src/components/template/Sidebar.vue
+++ cms/src/components/template/Sidebar.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="sidebar">
-
+    <ul>
+      <li>
+        <router-link to="/hooks" class="sidebarLink">
+          <span class="middle" />
+          <i class="el-icon-s-tools" />
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/channels" class="sidebarLink">
+          <span class="middle" />
+          <i class="el-icon-paperclip" />
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/messages" class="sidebarLink">
+          <span class="middle" />
+          <i class="el-icon-message" />
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -14,12 +33,30 @@
 </script>
 
 <style lang="scss" scoped>
-.sidebar {
-  background: #222;
-  position: fixed;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  width: 50px;
-}
+  .sidebar {
+    background: #222;
+    position: fixed;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    width: 50px;
+
+    > ul {
+      margin-top: 50px;
+    }
+
+    &Link {
+      display: block;
+      text-align: center;
+      height: 50px;
+      color: #fff;
+      font-size: 25px;
+
+      &:hover,
+      &.router-link-exact-active {
+        background: #a00;
+        color: #fff;
+      }
+    }
+  }
 </style>
