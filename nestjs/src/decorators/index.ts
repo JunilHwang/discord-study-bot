af812c7b01bc10d36189f8ed74f05a1d279ad048
junilhwang <tjsdlf4261@naver.com>
2020-07-15 23:19:13 +0900

    fix: [api] Decorator 오류 수정

--- nestjs/src/decorators/index.ts
+++ nestjs/src/decorators/index.ts
@@ -1,7 +1,8 @@
-import { createParamDecorator, UnauthorizedException } from '@nestjs/common'
+import {createParamDecorator, ExecutionContext, UnauthorizedException} from '@nestjs/common'
 import { Request } from 'express'
 
-export const Token = createParamDecorator((data: undefined, req: Request): string => {
+export const Token = createParamDecorator((data: undefined, ctx: ExecutionContext): string => {
+  const req: Request = ctx.switchToHttp().getRequest();
   const token: string|null = req.cookies.access_token || null
   if (token === null) throw new UnauthorizedException()
   return token
