6c09d804467fc97d31f2a29b2e50f23a39bed71e
junilhwang <tjsdlf4261@naver.com>
2020-07-16 23:36:48 +0900

    feat: [cms] GithubService에 fetchRepos 추가

--- cms/src/services/GithubService.ts
+++ cms/src/services/GithubService.ts
@@ -4,6 +4,7 @@ import $http from 'axios'
 const BASE_URL: string = '/api/github'
 const AUTH_URL: string = `${BASE_URL}/auth`
 const LOGOUT_URL: string = `${BASE_URL}/logout`
+const REPOS_URL: string = `${BASE_URL}/repos`
 
 export const fetchAuth = ({ id, password }: DefaultBody) =>
   $http.post(AUTH_URL, { id, password })
@@ -13,7 +14,6 @@ export const logout = () =>
   $http.post(LOGOUT_URL)
        .then(response => response.data)
 
-export default {
-  fetchAuth,
-  logout
-}
+export const fetchRepos = (id: string) =>
+  $http.get(`${REPOS_URL}/${id}`)
+       .then(response => response.data)
