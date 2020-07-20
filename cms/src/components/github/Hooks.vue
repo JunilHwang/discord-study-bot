2b2febf4732681ed015a685ff62e99d3b7ee97b9
junilhwang <tjsdlf4261@naver.com>
2020-07-21 01:43:28 +0900

    feat: [cms] Github Hook 목록 가져오는 작업 중 (4)

--- cms/src/components/github/Hooks.vue
+++ cms/src/components/github/Hooks.vue
@@ -2,11 +2,48 @@
   <el-card class="hooks">
     <h2 slot="header">Github Hooks</h2>
     <p v-if="selectedHook === null">
-      Repository를 선택하여 Hooks를 조회해주세요.
+      해당 Repository에 Webhook이 존재하지 않습니다.
     </p>
-    <ul>
+    <ul v-else class="hooksInfo">
       <li>
-        {{ selectedHook }}
+        <strong>type</strong>
+        <p v-html="selectedHook.type" />
+      </li>
+      <li>
+        <strong>id</strong>
+        <p v-html="selectedHook.id" />
+      </li>
+      <li>
+        <strong>name</strong>
+        <p v-html="selectedHook.name" />
+      </li>
+      <li>
+        <strong>active</strong>
+        <p v-html="selectedHook.active" />
+      </li>
+      <li>
+        <strong>events</strong>
+        <p v-html="selectedHook.events" />
+      </li>
+      <li>
+        <strong>updated_at</strong>
+        <p v-html="selectedHook.updated_at" />
+      </li>
+      <li>
+        <strong>created_at</strong>
+        <p v-html="selectedHook.created_at" />
+      </li>
+      <li>
+        <strong>url</strong>
+        <p v-html="selectedHook.url" />
+      </li>
+      <li>
+        <strong>test_url</strong>
+        <p v-html="selectedHook.test_url" />
+      </li>
+      <li>
+        <strong>ping_url</strong>
+        <p v-html="selectedHook.ping_url" />
       </li>
     </ul>
   </el-card>
@@ -29,5 +66,18 @@
 <style lang="scss" scoped>
   .hooks {
     margin-top: 30px;
+
+    &Info {
+      li {
+        display: flex;
+        font-size: 15px;
+        padding: 5px 0;
+      }
+      strong {
+        width: 120px;
+        color: #06F;
+        text-transform: uppercase;
+      }
+    }
   }
 </style>
