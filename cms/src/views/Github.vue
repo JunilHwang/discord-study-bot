bf67eb4f5c3085c613d0f677d92226d60231e4b5
junilhwang <tjsdlf4261@naver.com>
2020-07-21 00:29:43 +0900

    feat: [cms] Repository 체크박스 작업 중 (4)

--- cms/src/views/Github.vue
+++ cms/src/views/Github.vue
@@ -1,23 +1,27 @@
 <template>
   <section>
     <h1>Github Hook 관리</h1>
-    <el-card>
+    <el-card class="repositories">
       <h2 slot="header">Github Repository 목록</h2>
-      <el-tree
-        ref="$tree"
-        :data="reposTree"
-        :props="{ label: 'label' }"
-        @check-change="changeChecked"
-        node-key="id"
-        show-checkbox />
+      <p class="repositoryCount">
+        <strong v-html="repos.length + '개'" />의 Repository가 있습니다.
+      </p>
+      <ul class="clearFix">
+        <li v-for="repo in repos" :key="repo.id">
+          <label>
+            <input type="checkbox" />
+            <span>{{ repo.full_name }}</span>
+          </label>
+        </li>
+      </ul>
     </el-card>
   </section>
 </template>
 
 <script lang="ts">
-  import {Component, Vue} from 'vue-property-decorator'
-  import {GithubTinyRepository} from "domain/src"
-  import {namespace} from "vuex-class"
+  import {Component, Vue} from 'vue-property-decorator';
+  import {GithubTinyRepository} from "domain/src";
+  import {namespace} from "vuex-class";
   import {ActionMethod} from "vuex";
 
   const githubStore = namespace('githubStore');
@@ -25,24 +29,39 @@
 
   @Component
   export default class Hooks extends Vue {
-    @githubStore.State repos!: GithubTinyRepository[]
-    @userStore.Getter id!: string
-    @githubStore.Action FETCH_REPOS!: ActionMethod
-
-    get reposTree (): { label: string }[] {
-      return this.repos.map(repo => ({
-        label: repo.full_name
-      }))
-    }
-
-    changeChecked ({ label }: { [k: string]: string }, checked: boolean, indeterminate: boolean) {
-      console.log(label, checked, indeterminate);
-      const $tree: any = this.$refs.$tree;
-      console.log($tree.getCheckedNodes());
-    }
+    @githubStore.State repos!: GithubTinyRepository[];
+    @userStore.Getter id!: string;
+    @githubStore.Action FETCH_REPOS!: ActionMethod;
 
     created (): void {
       this.FETCH_REPOS(this.id);
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .repositoryCount {
+    margin-bottom: 20px;
+    font-size: 17px;
+  }
+  li {
+    float: left;
+    width: 50%;
+    line-height: 1;
+    margin-bottom: 10px;
+  }
+  strong {
+    color: #06F;
+  }
+  label {
+    cursor: pointer;
+    display: block;
+    span {
+      display: inline-block;
+      vertical-align: middle;
+      margin-left: 5px;
+      position: relative;
+      top: -1px;
+    }
+  }
+</style>
