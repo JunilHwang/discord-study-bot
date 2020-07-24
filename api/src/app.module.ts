ae29c479db91308d34c7ee8569f31253888e801a
junilhwang <tjsdlf4261@naver.com>
2020-07-24 19:47:23 +0900

    feat: [cms] IP 주소를 반환하는 API 제거

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,7 +4,6 @@ import {AppService} from './app.service';
 import {GithubModule} from "./api/github/github.module";
 import {DiscordModule} from "./api/discord/discord.module";
 import {APP_INTERCEPTOR} from "@nestjs/core";
-import {CommonModule} from "./api/common/common.module";
 
 const appCacheInterceptor = {
   provide: APP_INTERCEPTOR,
@@ -12,7 +11,7 @@ const appCacheInterceptor = {
 }
 
 @Module({
-  imports: [DiscordModule, GithubModule, CommonModule, CacheModule.register()],
+  imports: [DiscordModule, GithubModule, CacheModule.register()],
   controllers: [AppController],
   providers: [AppService, appCacheInterceptor],
 })
